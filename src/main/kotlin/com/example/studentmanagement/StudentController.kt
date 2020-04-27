58e54886d1beee6a8a275cc8f717a3c2765f0c01
sumanraj7261 <sumanraj@thoughtworks.com>
2020-04-27 21:04:30 +0530

    Added post api to save student data

--- src/main/kotlin/com/example/studentmanagement/StudentController.kt
+++ src/main/kotlin/com/example/studentmanagement/StudentController.kt
@@ -14,4 +14,10 @@ class StudentController(
     fun list(): ResponseEntity<List<Student>> {
         return ResponseEntity.ok(studentService.getStudents())
     }
+
+    @PostMapping("/add")
+    fun addStudent(@RequestParam name: String, @RequestParam age: Int, @RequestParam course: String): String{
+        studentService.addStudent(name, age, course)
+        return "Entry is successful with name $name age $age and course $course"
+    }
 }
--- src/main/kotlin/com/example/studentmanagement/StudentService.kt
+++ src/main/kotlin/com/example/studentmanagement/StudentService.kt
@@ -1,6 +1,9 @@
 package com.example.studentmanagement
 
 
+import com.mongodb.MongoClient
+import com.mongodb.client.MongoCollection
+import org.bson.Document
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,6 +13,16 @@ class StudentService {
     private val student3 = Student("third-name", 22, "MSC");
     private val student4 = Student("fourth-name", 23, "BCA");
     private val students:List<Student> = listOf(student1, student2, student3, student4)
+
+    fun getMongoCollection(): MongoCollection<Document>{
+        val mongoClient = MongoClient("localhost", 27017)
+        return  mongoClient.getDatabase("student").getCollection("list")
+    }
+
+    fun addStudent(name: String, age: Int, course: String) {
+        val collection = getMongoCollection()
+        collection.insertOne(org.bson.Document(mapOf("name" to name, "age" to age, "course" to course)))
+    }
 
     fun getStudents(): List<Student> {
         return students
