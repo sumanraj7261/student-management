f5ed58c885e99a0ef64cfecdc66090e19d070c51
sumanraj7261 <sumanraj@thoughtworks.com>
2020-05-04 03:26:16 +0530

    Added tests for getStudent method

--- src/test/kotlin/com/example/studentmanagement/StudentServiceTest.kt
+++ src/test/kotlin/com/example/studentmanagement/StudentServiceTest.kt
@@ -1,13 +1,46 @@
 package com.example.studentmanagement
 
+import com.mongodb.client.FindIterable
 import com.mongodb.client.MongoCollection
-import io.mockk.*
+import com.mongodb.client.MongoCursor
+import io.mockk.every
+import io.mockk.mockk
 import org.bson.Document
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.data.mongodb.core.MongoTemplate
 
 class StudentServiceTest{
+
+    @Test
+    fun `should return a list of student's data`() {
+        val mongoTemplate = mockk<MongoTemplate>()
+        val mongoCollection = mockk<MongoCollection<Document>>()
+        val iterable = mockk<FindIterable<Document>>()
+        val cursor = mockk<MongoCursor<Document>>()
+
+        val studentService = StudentService(mongoTemplate)
+
+        every {
+            mongoTemplate.db.getCollection("list")
+        } returns mongoCollection
+
+        every {
+            mongoCollection.find(Document(mapOf()))
+        } returns iterable
+
+        every {
+            iterable.iterator()
+        } returns cursor
+
+        every {
+            cursor.hasNext()
+        }  returns false
+
+        val emptylist = listOf<Map<String, Any>>()
+        assertEquals(emptylist, studentService.getStudents())
+    }
+
     @Test
     fun `should add one entry of student`() {
         val mongoTemplate = mockk<MongoTemplate>()
